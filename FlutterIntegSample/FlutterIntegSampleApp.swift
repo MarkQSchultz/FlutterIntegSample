eee9000dace7e50947dc2b6b3bc5638e58c94507
Mark Schultz <mark.schultz@emberhome.com>
2023-09-02 12:07:51 -1000

    Initial Commit

---
+++ FlutterIntegSample/FlutterIntegSampleApp.swift
@@ -0,0 +1,12 @@
+// FlutterIntegSampleApp.swift, created in 2023 for Ember
+
+import SwiftUI
+
+@main
+struct FlutterIntegSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
